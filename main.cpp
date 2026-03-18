eb6f37ff9b71ff2378eca3a4fdf92cab87e9f62f
rem_home <cgrem78@yandex.ru>
2026-03-18 12:34:42 +0300

    testadd

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//проверка кириллицы
+#include <iostream>
+#include <cstdlib> // для system
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Привет, мир" << endl;
+    system("pause");
+    return 0;
+}
