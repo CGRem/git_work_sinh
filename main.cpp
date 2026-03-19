a30f664ee2f4021327c580ae6eb61767ab53105b
rem_home <cgrem78@yandex.ru>
2026-03-19 20:24:01 +0300

    вихрь Мерсенна

--- main.cpp
+++ main.cpp
@@ -2,13 +2,20 @@
 #include <ctime>
 #include <iostream>
 #include <cstdlib> // для system
+#include <random>
 #include "function.h"
 //#include <string>
 using namespace std;
-int main()
+int main(int argc, char* argv[])
 {
     srand(time(0));
     setlocale(LC_ALL, "");
+
+    std::random_device rd;  // Источник энтропии
+    std::mt19937 gen(rd()); // Mersenne Twister, seed из random_device (вихрь Мерсенна)
+    std::uniform_int_distribution<> distrib(1, 1000);
+    for (int n = 0; n < 100; ++n) { std::cout << distrib(gen) << std::endl; };
+
     cout << "Привет мир" << endl;
     int rand_int = rand();
     cout << rand_int << endl;
