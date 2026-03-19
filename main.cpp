b99e2a0a31af6640febcb1a44dc896bbf584d305
Rem <cgrem78@yandex.ru>
2026-03-19 12:03:57 +0300

    random_w

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 ﻿//проверка кириллицы
+#include <ctime>
 #include <iostream>
 #include <cstdlib> // для system
 //#include <string>
 using namespace std;
 int main()
 {
+    srand(time(0));
     setlocale(LC_ALL, "");
     cout << "Привет мир" << endl;
-    system("pause");
+    int rand_int = rand();
+    cout << rand_int << endl;
+    //system("pause");
     return 0;
 }
