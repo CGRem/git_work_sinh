07e074ef9517bc231513627b1628972aefd0992c
Rem <cgrem78@yandex.ru>
2026-03-20 17:38:50 +0300

    vortex

--- main.cpp
+++ main.cpp
@@ -14,12 +14,10 @@ int main(int argc, char* argv[])
     std::random_device rd;  // Источник энтропии
     std::mt19937 gen(rd()); // Mersenne Twister, seed из random_device (вихрь Мерсенна)
     std::uniform_int_distribution<> distrib(1, 1000);
-    for (int n = 0; n < 100; ++n) { std::cout << distrib(gen) << std::endl; };
-
-    cout << "Привет мир" << endl;
-    int rand_int = rand();
-    cout << rand_int << endl;
-    cout << func_r() << endl;
-    //system("pause");
+    /*for (int n = 0; n < 100; ++n) { std::cout << distrib(gen) << std::endl; };*/
+    
+    int random_Int = distrib(gen);
+    cout << random_Int << endl;
+    system("pause");
     return 0;
 }
